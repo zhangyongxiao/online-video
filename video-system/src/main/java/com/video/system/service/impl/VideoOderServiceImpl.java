a70c72cbd0a97bfb9d45df5bb0fc0e02245044a9
zhangyongxiao <1055148679@qq.com>
2024-09-03 20:31:40 +0800

    theamleaf

--- video-system/src/main/java/com/video/system/service/impl/VideoOderServiceImpl.java
+++ video-system/src/main/java/com/video/system/service/impl/VideoOderServiceImpl.java
@@ -5,9 +5,11 @@ import com.video.system.domain.VideoOrder;
 import com.video.system.mapper.VideoOrderMapper;
 import com.video.system.service.VideoOderService;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 @Log4j2
@@ -18,8 +20,14 @@ public class VideoOderServiceImpl extends ServiceImpl<VideoOrderMapper, VideoOrd
     @Override
     public List<VideoOrder> listVideOrder() {
         log.info("-----");
-        List<VideoOrder> list =  this.lambdaQuery().eq(VideoOrder::getStatus,5)
-                .list();
-        return Collections.emptyList();
+        List<VideoOrder> list = new ArrayList<>();// this.lambdaQuery().eq(VideoOrder::getStatus,5)
+                //.list();
+        VideoOrder videoOrder =null;
+        for(int i=0;i<100;i++){
+            videoOrder = new VideoOrder();
+            videoOrder.setVideoName("视频"+i);
+            list.add(videoOrder);
+        }
+        return list;
     }
 }
